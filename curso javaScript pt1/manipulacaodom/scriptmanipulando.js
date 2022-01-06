7ee0c6e0b9d5ac7bc1bbbac1b3eef729705cad7a
Samuel souza <82341101+Samuel0802@users.noreply.github.com>
2022-01-06 17:19:54 -0400

    Add files via upload

---
+++ curso javaScript pt1/manipulacaodom/scriptmanipulando.js
@@ -0,0 +1,18 @@
+
+var p = document.getElementsByTagName('p');
+
+//alert(p[2].innerHTML);
+
+//p[0].innerHTML = 'Manipulação JS';
+
+
+/*
+for(var i = 0; i < p.length;i++){
+    p[i].innerHTML = 'Manipulação - ' +i;
+}
+*/
+
+
+for(var i = 0; i < 10; i++){
+    p[0].innerHTML = p[0].innerHTML +  ' Samuel ' ;
+}
